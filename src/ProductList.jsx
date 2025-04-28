000663beb57516d9867813039be499c71c9c16c4
Omair Tahir <omairutm@gmail.com>
2025-04-28 17:20:56 -0400

    Changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
-import CartItem from './CartItem';
-import addItem from './CartSlice';
+import {CartItem} from './CartItem';
+import {addItem} from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -284,26 +284,26 @@ function ProductList({ onHomeClick }) {
             {!showCart ? (
                 <div className="product-grid">
                     {plantsArray.map((category, index) => (
-                    <div key={index}>
-                        <h1><div>{category.category}</div></h1>
-                        <div className="product-list">
-                            {category.plants.map((plant, plantIndex) => (
-                                <div className="product-card" key={plantIndex}>
-                                    <img className="product-image" src={plant.image} alt={plant.name} />
-                                    <div className="product-title">{plant.name}</div>
-                                    <div className="product-description">{plant.description}</div>
-                                    <div className="product-price">{plant.cost}</div>
-                                    <button className={ addedToCart[`${categoryIndex}-${plantIndex}`] ? "product-button btn-disabled": "product-button"}
-                                        onClick={() => handleAddToCart(categoryIndex, plantIndex, plant)}
-                                        disabled={addedToCart[`${categoryIndex}-${plantIndex}`]}
-                                    >
-                                    Add to Cart
-                                    </button>
-                                </div>
-                            )                        </div>
+                        <div key={index}>
+                            <h1><div>{category.category}</div></h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex) => (
+                                    <div className="product-card" key={plantIndex}>
+                                        <img className="product-image" src={plant.image} alt={plant.name} />
+                                        <div className="product-title">{plant.name}</div>
+                                        <div className="product-description">{plant.description}</div>
+                                        <div className="product-price">{plant.cost}</div>
+                                        <button className={ addedToCart[`${categoryIndex}-${plantIndex}`] ? "product-button btn-disabled": "product-button"}
+                                            onClick={() => handleAddToCart(categoryIndex, plantIndex, plant)}
+                                            disabled={addedToCart[`${categoryIndex}-${plantIndex}`]}
+                                        >
+                                        Add to Cart
+                                        </button>
+                                    </div>
+                                )                        </div>
                 </div>
-                ))}
-                </div>
+                            ))}
+                    </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
             )}
